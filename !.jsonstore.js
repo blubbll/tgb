b6de8da26dd70d16b7e886fa190280c836189a3a
Glitch (tgb) <none>
2020-01-05 07:00:08 +0000

    👛📻 Updated with Glitch

--- !.jsonstore.js
+++ !.jsonstore.js
@@ -1,58 +1,73 @@
 const fetch = require("node-fetch");
-(function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define(factory) :
-    (global = global || self, global.jsonstore = factory());
-}(this, (function () { 'use strict';
+(function(global, factory) {
+  typeof exports === "object" && typeof module !== "undefined"
+    ? (module.exports = factory())
+    : typeof define === "function" && define.amd
+    ? define(factory)
+    : ((global = global || self), (global.jsonstore = factory()));
+})(this, function() {
+  "use strict";
 
-    const hostname = 'https://www.jsonstore.io';
+  const hostname = "https://www.jsonstore.io";
 
-    const getAccessKey = (accessKey) => {
-        try {
-            const parsedUrl = new URL(accessKey);
-            return parsedUrl.pathname.substring(1);
-        } catch(e) {
-            return accessKey;
-        }
-    };
+  const getAccessKey = accessKey => {
+    try {
+      const parsedUrl = new URL(accessKey);
+      return parsedUrl.pathname.substring(1);
+    } catch (e) {
+      return accessKey;
+    }
+  };
 
-    var index = (accessKey) => ({
-        get: (key) =>
-            fetch(`${hostname}/${getAccessKey(accessKey)}/${key}`)
-                .then(re => {console.log(re)}),
-                //.then((response) => response.json())
-                //.then((response) => response.ok ? response.result : Promise.reject(response.error)),
+  var index = accessKey => ({
+    get: key =>
+      fetch(`${hostname}/${getAccessKey(accessKey)}/${key}`)
+        //.then(re => {console.log(re)}),
+        .then(response => response.json())
+        .then(response =>
+          response.ok ? response.result : Promise.reject(response.error)
+        ),
 
-        post: (key, data) =>
-            fetch(`${hostname}/${getAccessKey(accessKey)}/${key}`, {
-                headers: {
-                    'Content-type': 'application/json'
-                },
+    post: (key, data) =>
+      fetch(`${hostname}/${getAccessKey(accessKey)}/${key}`, {
+        headers: {
+          "Content-type": "application/json"
+        },
 
-                method: 'POST',
-                body: JSON.stringify(data),
-            })
-                .then((response) => response.json())
-                .then((response) => response.ok ? Promise.resolve() : Promise.reject(response.error)),
+        method: "POST",
+        body: JSON.stringify(data)
+      })
+        //.then(re => {
+        //   console.log(re);
+        //  }),
+        .then(response => response.json())
+        .then(response =>
+          response.ok ? Promise.resolve() : Promise.reject(response.error)
+        ),
 
-        put: (key, data) =>
-            fetch(`${hostname}/${getAccessKey(accessKey)}/${key}`, {
-                headers: {
-                    'Content-type': 'application/json'
-                },
+    put: (key, data) =>
+      fetch(`${hostname}/${getAccessKey(accessKey)}/${key}`, {
+        headers: {
+          "Content-type": "application/json"
+        },
 
-                method: 'PUT',
-                body: JSON.stringify(data),
-            })
-                .then((response) => response.json())
-                .then((response) => response.ok ? Promise.resolve() : Promise.reject(response.error)),
+        method: "PUT",
+        body: JSON.stringify(data)
+      })
+        .then(response => response.json())
+        .then(response =>
+          response.ok ? Promise.resolve() : Promise.reject(response.error)
+        ),
 
-        delete: (key) =>
-            fetch(`${hostname}/${getAccessKey(accessKey)}/${key}`, { method: 'DLETE' })
-                .then((response) => response.json())
-                .then((response) => response.ok ? Promise.resolve() : Promise.reject(response.error)),
-    });
+    delete: key =>
+      fetch(`${hostname}/${getAccessKey(accessKey)}/${key}`, {
+        method: "DLETE"
+      })
+        .then(response => response.json())
+        .then(response =>
+          response.ok ? Promise.resolve() : Promise.reject(response.error)
+        )
+  });
 
-    return index;
-
-})));
+  return index;
+});
--- server.js
+++ server.js
@@ -40,24 +40,119 @@ app.post(`${prefix}/msg`, async (req, res) => {
 
   // console.log(message);
 
-  const wapContext = await Store.get(`wac/${message.chat.id}`);
+  let waChats = await Store.get(`waChats`);
+  if (waChats === null) {
+    waChats = {};
+    await Store.post(`waChats`, waChats);
+    console.log("WaChats initiated.");
+  } else waChats = JSON.parse(waChats);
 
-  console.log(wapContext);
+  const key = `${message.chat.id}`;
+  const reqChat = waChats[key];
+
+  //chat destination
+  let dest = "";
+  if (reqChat === void 0) {
+    const newChat = {
+      waId: message.sender.id,
+      group: message.isGroupMsg,
+      nick: message.isGroupMsg
+        ? message.chat.contact.formattedName
+        : message.chat.contact.pushname,
+      no: message.isGroupMsg
+        ? message._groupOwner.formattedName
+        : message.chat.contact.formattedName
+    };
+
+    waChats[key] = newChat;
+
+    await Store.post(`waChats`, JSON.stringify(waChats));
+    console.log("waChats updated.");
+    dest = process.env.OWNER;
+  } else {
+    dest = reqChat.tId === null ? process.env.OWNER : reqChat.tId;
+  }
+
+  //Store.post("waChats", {})
+  //console.log(waChats);
 
   //console.log(await telegram.getMe());
 
   //console.log(message);
 
   switch (message.type) {
+    case "chat":
+      {
+        //pm
+        if (!message.chat.isGroup) {
+          const waAccount = {
+            chatId: message.chat.id,
+            accName: message.sender.pushname,
+            number: message.sender.formattedName
+          };
+
+          // console.log(waAccount);
+
+          telegram.sendMessage(
+            dest,
+            reqChat.tId === null
+              ? `💬 Neue Nachricht von\n${message.sender.pushname}<${
+                  message.sender.id
+                }>\n\t${"_".repeat(28)}\n\n${message.body}`
+              : message.body,
+            {
+              disable_notification: 1
+            }
+          );
+        } else {
+          //Gruppe
+          const waGroup = {
+            chatId: message.chat.id,
+            name: message.chat.contact.formattedName,
+            desc: message.chat.groupMetadata.desc,
+            tId: null
+          };
+
+          telegram.sendMessage(
+            dest,
+            reqChat.tId === null
+              ? `📚 Neue Nachricht in Gruppe ${waGroup.name}<${
+                  waGroup.chatId
+                }> von ${message.sender.pushname}<${
+                  message.sender.formattedName
+                }>
+              ${"_".repeat(30)}\n\n${message.body}`
+              : `📚 ${message.sender.pushname}<${message.sender.formattedName}>:\n\n${message.body}`,
+            {
+              disable_notification: 1
+            }
+          );
+
+          //console.log(waGroup);
+        }
+      }
+      break;
     case "image":
       {
-        await telegram.sendMessage(
-          process.env.OWNER,
-          `📷 Neues Bild von\n${message.chat.contact.pushname}<${message.chat.contact.id}>:`,
-          {
-            disable_notification: 1
-          }
-        );
+        if (message.chat.isGroup) {
+          await telegram.sendMessage(
+            dest,
+            `📷 Neues Bild von\n${message.chat.contact.pushname}:`,
+            {
+              disable_notification: 1
+            }
+          );
+        } else {
+          await telegram.sendMessage(
+            dest,
+            reqChat.tId === null
+              ? `📷 Neues Bild von\n${message.chat.contact.pushname}<${message.chat.contact.id}>:`
+              : `📷 Neues Bild:`,
+            {
+              disable_notification: 1
+            }
+          );
+        }
         base64Img.img(
           `data:${message.mimetype};base64,${message.body}`,
           `${__dirname}/tmp`,
@@ -65,7 +160,7 @@ app.post(`${prefix}/msg`, async (req, res) => {
           async (err, filepath) => {
             console.log(filepath);
             const t = filepath;
-            await telegram.sendPhoto(process.env.OWNER, {
+            await telegram.sendPhoto(dest, {
               source: fs.readFileSync(t)
             });
             fs.unlinkSync(filepath);
@@ -76,7 +171,7 @@ app.post(`${prefix}/msg`, async (req, res) => {
     case "video":
       {
         await telegram.sendMessage(
-          process.env.OWNER,
+          dest,
           `🎬 Neues Video von\n${message.chat.contact.pushname}<${message.chat.contact.id}> (nur Thumbnail):`,
           {
             disable_notification: 1
@@ -89,7 +184,7 @@ app.post(`${prefix}/msg`, async (req, res) => {
           async (err, filepath) => {
             console.log(filepath);
             const t = filepath;
-            await telegram.sendPhoto(process.env.OWNER, {
+            await telegram.sendPhoto(dest, {
               source: fs.readFileSync(t)
             });
             fs.unlinkSync(filepath);
@@ -100,7 +195,7 @@ app.post(`${prefix}/msg`, async (req, res) => {
     case "sticker":
       {
         await telegram.sendMessage(
-          process.env.OWNER,
+          dest,
           `📎 Neuer Sticker von\n${message.chat.contact.pushname}<${message.chat.contact.id}>(protected...)`,
           {
             disable_notification: 1
@@ -111,7 +206,7 @@ app.post(`${prefix}/msg`, async (req, res) => {
     case "audio":
       {
         await telegram.sendMessage(
-          process.env.OWNER,
+          dest,
           `🎵 Neues Audio von\n${message.chat.contact.pushname}<${message.chat.contact.id}>(protected...)`,
           {
             disable_notification: 1
@@ -122,7 +217,7 @@ app.post(`${prefix}/msg`, async (req, res) => {
     case "ptt":
       {
         await telegram.sendMessage(
-          process.env.OWNER,
+          dest,
           `🔈 Neues voice memo von\n${message.chat.contact.pushname}<${message.chat.contact.id}>(protected...)`,
           {
             disable_notification: 1
@@ -151,55 +246,26 @@ app.post(`${prefix}/msg`, async (req, res) => {
           }
         );*/
         await telegram.sendMessage(
-          process.env.OWNER,
+          dest,
           `📍 Neuer Ort von\n${message.chat.contact.pushname}<${message.chat.contact.id}>:`
         );
-        telegram.sendLocation(process.env.OWNER, message.lat, message.lng);
+        telegram.sendLocation(dest, message.lat, message.lng);
       }
       break;
-    case "chat":
-      {
-        if (!message.chat.isGroup) {
-          const waAccount = {
-            chatId: message.chat.id,
-            accName: message.sender.pushname,
-            number: message.sender.formattedName
-          };
+  }
 
-          // console.log(waAccount);
-
-          telegram.sendMessage(
-            process.env.OWNER,
-            `💬 Neue Nachricht von\n${message.sender.pushname}<${
-              message.sender.id
-            }>\n\t${"_".repeat(28)}\n\n${message.body}`,
-            {
-              disable_notification: 1
-            }
-          );
-        } else {
-          //Gruppe
-          const waGroup = {
-            chatId: message.chat.id,
-            name: message.chat.contact.formattedName,
-            desc: message.chat.groupMetadata.desc
-          };
-
-          telegram.sendMessage(
-            process.env.OWNER,
-            `📚 Neue Nachricht in Gruppe ${waGroup.name}<${
-              waGroup.chatId
-            }> von ${message.sender.pushname}<${message.sender.formattedName}>
-              ${"_".repeat(30)}\n\n${message.body}`,
-            {
-              disable_notification: 1
-            }
-          );
-
-          //console.log(waGroup);
-        }
+  //only reply to pms
+  if (!message.isGroupMsg) {
+    const m = "✓ Nachricht wurde weitergeleitet";
+    emitter.emit("event", {
+      type: "msg",
+      data: {
+        to: key,
+        text: `✓ 𝘕𝘢𝘤𝘩𝘳𝘪𝘤𝘩𝘵 𝘸𝘶𝘳𝘥𝘦 𝘸𝘦𝘪𝘵𝘦𝘳𝘨𝘦𝘭𝘦𝘪𝘵𝘦𝘵. \n${transform(
+          "Melde mich später…"
+        )}\n~ℝ𝕦𝕓𝕖𝕟`
       }
-      break;
+    });
   }
 
   res.end();
@@ -217,54 +283,87 @@ app.post("/:token/:type/:number", (req, res) => {});
   //console.log(await telegram.getMe());
 })();
 
-var browserChannel = require("browserchannel").server;
-//browserchannel
-app.use(
-  browserChannel(
-    {
-      base: `/channel`
-    },
-    session => {
-      console.log(
-        "New session: " +
-          session.id +
-          " from " +
-          session.address +
-          " with cookies " +
-          session.headers.cookie
-      );
-      session.on("message", data => {
-        console.log(data);
-        session.send();
-      });
-      session.on("close", reason => {
-        console.log(session.id + " disconnected (" + reason + ")");
-      });
-    }
-  )
-);
+const EventEmitter = require("eventemitter3");
+const emitter = new EventEmitter();
 
-const numsDir = `${__dirname}/conf/nums`;
-!fs.existsSync(numsDir) && fs.mkdirSync(numsDir);
+app.get(`/events/${process.env.WAE_TOKEN}`, cors(), (req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive"
+  });
+
+  // Heartbeat
+  const nln = () => {
+    res.write("\n");
+  };
+  const hbt = setInterval(nln, 15000);
+
+  const onEvent = data => {
+    res.write("retry: 500\n");
+    res.write(`event: event\n`);
+    res.write(`data: ${JSON.stringify(data)}\n\n`);
+  };
+
+  emitter.on("event", onEvent);
+
+  // Clear heartbeat and listener
+  req.on("close", () => {
+    clearInterval(hbt);
+    emitter.removeListener("event", onEvent);
+  });
+});
+
+setInterval(() => {
+  //emitter.emit('event', {ping: "test"});
+}, 4999);
 
 bot.on("message", async ctx => {
   const msg = ctx.update.message;
   const txt = msg.text;
 
+  // emitter.emit('event', {ping: "test"});
+
   if (txt && txt.startsWith("/")) {
     const args = txt.split(" ")[1];
     switch (txt.split(" ")[0]) {
-      case "/num": {
-        const num = args;
-        fs.writeFileSync(`${numsDir}/${num}`, 1, {
-          num: num,
-          group: msg.chat
-        });
+      case "/set": {
+        const key = args;
+
+        let waChats = await Store.get(`waChats`);
+        if (waChats === null) {
+          waChats = {};
+          await Store.post(`waChats`, waChats);
+          console.log("WaChats initiated.");
+        } else waChats = JSON.parse(waChats);
+
+        const reqChat = waChats[key];
+        //update telegram id
+
+        const _tId = reqChat.tId;
+        reqChat.tId = msg.chat.id;
+
+        await Store.post(`waChats`, JSON.stringify(waChats));
+        console.log("waChats updated.");
+
+        //only notify if not in db already with diff number
+        _tId === undefined &&
+          emitter.emit("event", {
+            type: "msg",
+            data: {
+              to: key,
+              text: `🤖 𝘕𝘢𝘤𝘩𝘳𝘪𝘤𝘩𝘵𝘦𝘯 𝘸𝘦𝘳𝘥𝘦𝘯 𝘢𝘣 𝘫𝘦𝘵𝘻𝘵 𝘸𝘦𝘪𝘵𝘦𝘳𝘨𝘦𝘭𝘦𝘪𝘵𝘦𝘵.`
+            }
+          });
 
         try {
+          //console.log(reqChat);
+          await telegram.setChatTitle(msg.chat.id, `📞~${reqChat.nick}`); //telefon icon
           await telegram.setChatDescription(
             msg.chat.id,
-            `WhatsApp-Gateway für Nummer +${num}.`
+            `WhatsApp-Gateway für Nummer ${reqChat.no}\nWa-Account[${
+              reqChat.waId
+            }].\nLetztes Update:\n${new Date()}`
           );
           ctx.reply("Gateway-Konfiguration erfolgreich aktualisiert!\n✅");
         } catch (e) {
@@ -272,7 +371,33 @@ bot.on("message", async ctx => {
         }
       }
     }
-  } else console.log(ctx.update.message);
+  } else {
+    let waChats = await Store.get(`waChats`);
+    if (waChats === null) {
+      waChats = {};
+      await Store.post(`waChats`, waChats);
+      console.log("WaChats initiated.");
+    } else waChats = JSON.parse(waChats);
+
+    console.log();
+    let reqChat;
+    for (const [key, value] of Object.entries(waChats)) {
+      if (waChats[key].tId === msg.chat.id) {
+        reqChat = waChats[key];
+        break;
+      }
+    }
+
+    if (reqChat) {
+      emitter.emit("event", {
+        type: "msg",
+        data: {
+          to: reqChat.waId,
+          text: transform(txt)
+        }
+      });
+    }
+  }
   //console.log(ctx.update.message.text)
 
   //console.log(ctx.update.message.from);
@@ -291,3 +416,79 @@ telegram.sendMessage(process.env.OWNER, "✅ Bot aktiv.", {
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+const map = new Map([
+  ["A", "𝖠"],
+  ["B", "𝖡"],
+  ["C", "𝖢"],
+  ["D", "𝖣"],
+  ["E", "𝖤"],
+  ["F", "𝖥"],
+  ["G", "𝖦"],
+  ["H", "𝖧"],
+  ["I", "𝖨"],
+  ["J", "𝖩"],
+  ["K", "𝖪"],
+  ["L", "𝖫"],
+  ["M", "𝖬"],
+  ["N", "𝖭"],
+  ["O", "𝖮"],
+  ["P", "𝖯"],
+  ["Q", "𝖰"],
+  ["R", "𝖱"],
+  ["S", "𝖲"],
+  ["T", "𝖳"],
+  ["U", "𝖴"],
+  ["V", "𝖵"],
+  ["W", "𝖶"],
+  ["X", "𝖷"],
+  ["Y", "𝖸"],
+  ["Z", "𝖹"],
+  ["a", "𝖺"],
+  ["b", "𝖻"],
+  ["c", "𝖼"],
+  ["d", "𝖽"],
+  ["e", "𝖾"],
+  ["f", "𝖿"],
+  ["g", "𝗀"],
+  ["h", "𝗁"],
+  ["i", "𝗂"],
+  ["j", "𝗃"],
+  ["k", "𝗄"],
+  ["l", "𝗅"],
+  ["m", "𝗆"],
+  ["n", "𝗇"],
+  ["o", "𝗈"],
+  ["p", "𝗉"],
+  ["q", "𝗊"],
+  ["r", "𝗋"],
+  ["s", "𝗌"],
+  ["t", "𝗍"],
+  ["u", "𝗎"],
+  ["v", "𝗏"],
+  ["w", "𝗐"],
+  ["x", "𝗑"],
+  ["y", "𝗒"],
+  ["z", "𝗓"],
+  ["0", "𝟢"],
+  ["1", "𝟣"],
+  ["2", "𝟤"],
+  ["3", "𝟥"],
+  ["4", "𝟦"],
+  ["5", "𝟧"],
+  ["6", "𝟨"],
+  ["7", "𝟩"],
+  ["8", "𝟪"],
+  ["9", "𝟫"]
+]);
+
+const transform = input => {
+  let output = "";
+  var regex = new RegExp("^[a-zA-Z0-9]+$");
+
+  input &&
+    input.split("").forEach(char => {
+      regex.test(char) ? [(output += map.get(char))] : (output += char);
+    });
+  return output;
+};
